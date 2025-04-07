76b0987b3d44b7977e61a137e161671839fb5a9a
Joeunji0119 <rell_cake@naver.com>
2025-04-07 14:23:51 +0900

    feat : sub 되는지 확인용

---
+++ src/Sub.tsx
@@ -0,0 +1,5 @@
+const Sub = () => {
+  return <div>SubPage</div>;
+};
+
+export default Sub;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+import Sub from './Sub.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      /** =====================  login & join =====================================*/
+      { path: '/', element: <App /> },
+      { path: '/sub', element: <Sub /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />;
+  </StrictMode>
+);
